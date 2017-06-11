8cae287d4277eaa95a6ca5b6af546517bf6e283a
utk1801 <utk1801@users.noreply.github.com>
2017-06-11 18:53:18 +0000

    Done

--- build.py
+++ build.py
@@ -2,18 +2,12 @@ import pandas as pd
 
 
 def equal_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+    return ds1==ds2
 
 
 def greater_than_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+    return ds1>ds2
 
 
 def less_than_operator(ds1, ds2):
-    """
-    Enter your code here
-    """
+    return ds1<ds2
